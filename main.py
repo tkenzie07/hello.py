b2e71019fac01449c4fa1f29eb3a51fc80c15d05
tkenzie07 <tkenzie77@gmail.com>
2024-08-30 18:03:06 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Perform the calculations and store the results in variables
+result1 = 5 * 6
+result2 = 8 / 3
+result3 = 8 // 3
+result4 = 8 % 3
+result5 = 4200000 + 23090
+result6 = 4200000 - 7633
 
+# Print the results with the required statements
+print(f"The answer to your query is {result1}")
+print(f"The answer to your query is {result2:.8f}")
+print(f"The answer to your query is {result3}")
+print(f"The answer to your query is {result4}")
+print(f"The answer to your query is {result5}")
+print(f"The answer to your query is {result6}")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f' {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('Hello World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Print my name as the last line of the code
+print("My name is Moonchild")
